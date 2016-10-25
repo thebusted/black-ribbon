0c7ff49c0d3f2656fda3298bb987a66dfd6b4c55
THEBUSTED <tksumeth@gmail.com>
2016-10-25 20:12:14 +0700

    Minify

--- black-ribbon.js
+++ black-ribbon.js
@@ -1,65 +1,7 @@
 /**
-* BlackRibbon
+* BlackRibbon BETA v1.0
+* 25 October 2016
+* https://github.com/thebusted/black-ribbon/
 */
-var BlackRibbon = (function () {
-	this.cdn = 'https://storage.googleapis.com/black-ribbon.appspot.com/img/';
-	this.getPosition = function () {
-		// Get option from window['brb']
-		var brb = window['brb'] || null;
-
-		// Return x and y
-		return {
-			x: (brb.x === 'left' || brb.x === 'right') ? brb.x : 'left',
-			y: (brb.y === 'top' || brb.y === 'bottom') ? brb.y : 'top'
-		};
-	};
-	this.getImage = function(pos, src){
-		return cdn + 'black_ribbon_' + pos.y + '_' + pos.x + '.png';
-	};
-	this.createBlackRibbon = function (pos) {
-		// Create img tag
-		var ribbon = document.createElement('img');
-		
-		// CSS coding by NUUNEOI.COM, Source: http://nuuneoi.com/blog/blog.php?read_id=884
-		// Black ribbon by Jatuporn Jib Piyawarinwong, Source: https://www.facebook.com/jibbazee/posts/10153967020642895
-		ribbon.src = getImage(pos);
-		ribbon.style.position = 'fixed';
-		ribbon.style.width = 70;
-		ribbon.style.zIndex = '9999';
-		
-		// Set x position
-		if(pos.x === 'right') {
-			ribbon.style.right = 0;
-		} else {
-			ribbon.style.left = 0;
-		}
-		
-		// Set y position
-		if(pos.y === 'bottom') {
-			ribbon.style.bottom = 0;
-		} else {
-			ribbon.style.top = 0;
-		}
-
-		return ribbon;
-	};
-	this.render = function () {
-		// Get position
-		var pos = getPosition();
-
-		// Create img tag with ribbon
-		var ribbon = createBlackRibbon(pos);
-
-		// Append image to body
-		document.body.appendChild(ribbon);
-	};
-
-	return {
-		init: function () {
-			render();
-		}
-	};
-})();
-
-// initialize Black Ribbon
-BlackRibbon.init();
+var BlackRibbon=function(){this.cdn="https://storage.googleapis.com/black-ribbon.appspot.com/img/";this.getPosition=function(){var a=window.brb||null;return{x:"left"===a.x||"right"===a.x?a.x:"left",y:"top"===a.y||"bottom"===a.y?a.y:"top"}};this.getImage=function(a,b){return cdn+"black_ribbon_"+a.y+"_"+a.x+".png"};this.createBlackRibbon=function(a){var b=document.createElement("img");b.src=getImage(a);b.style.position="fixed";b.style.width=70;b.style.zIndex="9999";"right"===a.x?b.style.right=0:b.style.left=
+0;"bottom"===a.y?b.style.bottom=0:b.style.top=0;return b};this.render=function(){var a=getPosition(),a=createBlackRibbon(a);document.body.appendChild(a)};return{init:function(){render()}}}();BlackRibbon.init();
